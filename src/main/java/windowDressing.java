5f2192c3309737273cc21fe4d78bf9bb255ba32a
SmithPatrick6000c <patdousmi@gmail.com>
2026-04-24 14:47:17 -0400

    Barebones window exists

--- src/main/java/windowDressing.java
+++ src/main/java/windowDressing.java
@@ -1,39 +1,81 @@
 
+import java.awt.*;
 import javax.swing.*;
 
 public class windowDressing {
 
-    private final int defOp = JFrame.EXIT_ON_CLOSE;
     private final int width = 400;
     private final int height = 300;
-    
-    
-    JFrame frame;
+
+    //Frame
+    private JFrame frame;
+
+    // How to switch screens
+    private CardLayout cardLayout;
+    private JPanel panelContainer;
+
+    //Panels
+    private JPanel panel1;
+    private JPanel panel2;
 
     public windowDressing()
     {
-        frame = new JFrame("Window?");
+        frame = new JFrame("Typing Game");
         initWindow();
-        
-
     }
 
     private void initWindow()
     {
-        frame.setDefaultCloseOperation(defOp);
-        frame.setSize(width, height);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(width,height);
         frame.setLocationRelativeTo(null);
+
+        cardLayout = new CardLayout();
+        panelContainer = new JPanel(cardLayout);
+
+        initPanel1();
+        initPanel2();
+
+        panelContainer.add(panel1, "menu");
+        panelContainer.add(panel2, "game");
+
+        frame.add(panelContainer);
+        
         frame.setVisible(true);
     }
 
-
     private void initPanel1()
     {
-        JPanel panel1 = new JPanel();
+        panel1 = new JPanel();
+        JButton button = new JButton("Start");
+        button.addActionListener(e -> showGame());
 
-        
+        JLabel label = new JLabel("Click the Button");
 
+
+        panel1.add(label);
+        panel1.add(button);
     }
+
+    private void initPanel2()
+    {
+        panel2 = new JPanel();
+
+        JTextField textField = new JTextField("Default text...");
+        textField.setPreferredSize(new Dimension(200, 30));
+
+        JLabel toType = new JLabel("You should Type this");
+
+        panel2.add(toType);
+        panel2.add(textField);
+    }
+
+    private void showGame()
+    {
+        cardLayout.show(panelContainer, "game");
+    }
+
+
 
 
 
